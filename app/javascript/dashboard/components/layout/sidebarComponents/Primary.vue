414865e20ee2e9a0583c57679ff203b8d8416062
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-05-25 01:20:20 -0300

    fix style

--- app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/Primary.vue
@@ -73,7 +73,7 @@ export default {
   <div
     class="flex flex-col justify-between w-16 h-full bg-white border-r dark:bg-slate-900 border-slate-50 dark:border-slate-800/50 rtl:border-l rtl:border-r-0"
   >
-    <div class="flex flex-col items-center gap-2">
+    <div class="flex flex-col items-center gap-2 mt-3">
       <!-- <Logo
         :source="logoSource"
         :name="installationName"
--- app/javascript/dashboard/components/layout/sidebarComponents/PrimaryNavItem.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/PrimaryNavItem.vue
@@ -95,7 +95,7 @@ export default {
   <OnClickOutside v-if="isHelpCenter" @trigger="showHelpCenterMenu = false">
     <button
       v-tooltip.top="$t(`SIDEBAR.${name}`)"
-      class="relative flex items-center justify-center w-10 h-10 my-2 rounded-lg text-slate-700 dark:text-slate-100 hover:!bg-slate-25 dark:hover:!bg-slate-700 dark:hover:text-slate-100 hover:text-slate-600"
+      class="relative flex items-center justify-center w-10 h-10 rounded-lg text-slate-700 dark:text-slate-100 hover:!bg-slate-25 dark:hover:!bg-slate-700 dark:hover:text-slate-100 hover:text-slate-600"
       :class="{
         'bg-woot-50 dark:bg-slate-800 text-woot-500 hover:bg-woot-50':
           isHelpCenterSelected,
@@ -121,7 +121,7 @@ export default {
     <a
       v-tooltip.right="$t(`SIDEBAR.${name}`)"
       :href="href"
-      class="relative flex items-center justify-center w-10 h-10 my-2 rounded-lg text-slate-700 dark:text-slate-100 hover:bg-slate-25 dark:hover:bg-slate-700 dark:hover:text-slate-100 hover:text-slate-600"
+      class="relative flex items-center justify-center w-10 h-10 rounded-lg text-slate-700 dark:text-slate-100 hover:bg-slate-25 dark:hover:bg-slate-700 dark:hover:text-slate-100 hover:text-slate-600"
       :class="{
         'bg-woot-50 dark:bg-slate-800 text-woot-500 hover:bg-woot-50':
           isActive || isChildMenuActive,
