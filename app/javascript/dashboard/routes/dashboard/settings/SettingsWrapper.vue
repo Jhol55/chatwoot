681ff9cf9b0cfface8fe01b6513be55ab368869a
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-06-05 15:09:24 -0300

    fix style

--- app/javascript/dashboard/routes/dashboard/settings/SettingsWrapper.vue
+++ app/javascript/dashboard/routes/dashboard/settings/SettingsWrapper.vue
@@ -11,7 +11,7 @@ defineProps({
   <div
     class="flex flex-col w-full h-full m-0 p-6 sm:py-8 lg:px-16 overflow-auto bg-n-background font-inter"
   >
-    <div class="flex items-start w-full max-w-6xl mx-auto">
+    <div class="flex items-start w-full h-full max-w-6xl mx-auto">
       <router-view v-slot="{ Component }">
         <keep-alive v-if="keepAlive">
           <component :is="Component" />
